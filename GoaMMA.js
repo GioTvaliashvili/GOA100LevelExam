d5b3f1de659fd0b43dac670fb8fbb3bf052bd5ac
GioTvaliashvili <babucidze86@gmail.com>
2026-01-31 17:51:06 +0400

    commit

---
+++ GoaMMA.js
@@ -0,0 +1,21 @@
+ document.querySelectorAll(".go-btn").forEach(button => {
+    button.addEventListener("click", () => {
+      window.location.href = button.dataset.url;
+    });
+  });
+
+
+   document.querySelectorAll(".go-btn1").forEach(button => {
+    button.addEventListener("click", () => {
+      window.location.href = button.dataset.url;
+    });
+  });
+
+     document.querySelectorAll(".go-btn2").forEach(button => {
+    button.addEventListener("click", () => {
+      window.location.href = button.dataset.url;
+    });
+  });
+
+
+
---
+++ HakatonandActives.js
@@ -0,0 +1,15 @@
+ document.querySelectorAll(".go-btn").forEach(button => {
+    button.addEventListener("click", () => {
+      window.location.href = button.dataset.url;
+    });
+  });
+
+
+   document.querySelectorAll(".go-btn1").forEach(button => {
+    button.addEventListener("click", () => {
+      window.location.href = button.dataset.url;
+    });
+  });
+
+
+
